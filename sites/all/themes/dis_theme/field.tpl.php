35c6756f3e110397d3f673566cc7852758457ce7
Ankur Agarwal <agarwalankur100@gmail.com>
2014-07-23 15:02:54 +0530

    Added field.tpl.php to DIS Theme to remove colon after field labels

--- sites/all/themes/dis_theme/field.tpl.php
+++ sites/all/themes/dis_theme/field.tpl.php
@@ -44,16 +44,9 @@
  * @ingroup themeable
  */
 ?>
-<!--
-THIS FILE IS NOT USED AND IS HERE AS A STARTING POINT FOR CUSTOMIZATION ONLY.
-See http://api.drupal.org/api/function/theme_field/7 for details.
-After copying this file to your theme's folder and customizing it, remove this
-HTML comment.
--->
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
-  <!-- Custom Changes - Removed colon after label -->
-    <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>&nbsp;</div>
+     <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>&nbsp;</div>
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
